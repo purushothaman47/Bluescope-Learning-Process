37c679e19ff2940f2fdbcf545595c69afa11c4d0
purushothaman47 <144509020+purushothaman47@users.noreply.github.com>
2025-11-18 16:05:23 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Swithcxample {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter day number 1-7: ");
+        int day = obj.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("invalid day");
+        }
+    }
+}
